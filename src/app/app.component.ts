e35a98dcb88ea314054869fa656a53bb01b3ce07
mantuonweb <mantu.nigam@globallogic.com>
2017-10-25 15:25:51 +0530

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+class Day{
+	cars: number;
+	constructor() {
+		// code...
+	}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  requesttypedata: any[] = [new Day()];
+  addMore(){
+	  this.requesttypedata.push(new Day())
+  }
 }
